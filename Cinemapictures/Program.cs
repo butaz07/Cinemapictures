782842e405e37ffe3ba7c19c26b0793bcd68a322
butaz07 <elianchavezfranco@gmail.com>
2025-08-17 12:24:31 -0400

    Update Program.cs

--- Cinemapictures/Program.cs
+++ Cinemapictures/Program.cs
@@ -1,19 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// En Program.cs
 builder.Services.AddDbContext<DataContex>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DataContex")));
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
